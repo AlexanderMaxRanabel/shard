2e7bb622d2b70802d63a4ec19533f08b11ae18c0
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-05-10 23:15:27 +0300

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+const crypto = require('crypto');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function generateRandomString(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+rl.question('Enter a string to hash: ', (data) => {
+    const data_hash1 = crypto.createHash('sha256').update(data).digest('hex');
+    const data_hash2 = crypto.createHash('sha256').update(data_hash1).digest('hex');
+    const data_hash3 = crypto.createHash('sha256').update(data_hash2).digest('hex');
+    
+    const randomInt = Math.floor(Math.random() * 255);
+    const rstring = generateRandomString(randomInt);
+
+    const target_hash = crypto.createHash('sha256').update(rstring).digest('hex');
+    rl.close();
+});
